d5cfc34091c668528e947016ae9edb5e08d768e7
harisrab <harisrab01@gmail.com>
2022-02-02 19:28:13 +0800

    Refactored

--- dog-e.ino
+++ dog-e.ino
@@ -1,22 +1,20 @@
 #include "imu.h"
 
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
 
 void setup()
 {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    // // join I2C bus (I2Cdev library doesn't do this automatically)
+    // #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
         TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
+    // #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    //     Fastwire::setup(400, true);
+    // #endif
 
     // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
     Serial.begin(115200);
-    
     while (!Serial)
         ; // wait for Leonardo enumeration, others continue immediately
 
@@ -26,28 +24,7 @@ void setup()
     // 38400 or slower in these cases, or use some kind of external separate
     // crystal solution for the UART timer.
 
-    
-
-    // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    // while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available())
-        ; // wait for data
-    // while (Serial.available() && Serial.read()); // empty buffer again
-
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(135);
-    mpu.setYGyroOffset(34);
-    mpu.setZGyroOffset(-30);
-    mpu.setZAccelOffset(1736); // 1688 factory default for my test chip
+    setupIMU();
 
     // make sure it worked (returns 0 if so)
     if (devStatus == 0)
@@ -116,7 +93,6 @@ void loop()
         //     cnt = 0;
         //     Serial.println(cnt1++);
         // }
-
     }
 
     // reset interrupt flag and get INT_STATUS byte
@@ -143,8 +119,6 @@ void loop()
 
         // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-
 
         // track FIFO count here in case there is > 1 packet available
         // (this lets us immediately read more without waiting for an interrupt)
@@ -231,8 +205,5 @@ void loop()
         Serial.write(teapotPacket, 14);
         teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
 #endif
-
-
-    
     }
 }
--- src/imu/imu.cpp
+++ src/imu/imu.cpp
@@ -1,4 +1,4 @@
-#include 
+#include "imu.h"
 
 void dmpDataReady()
 {
@@ -10,4 +10,24 @@ void setupIMU()
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready [By default doesn't wait]
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    // while (Serial.available() && Serial.read()); // empty buffer
+    // while (!Serial.available()) // wait for data
+    // while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(135);
+    mpu.setYGyroOffset(34);
+    mpu.setZGyroOffset(-30);
+    mpu.setZAccelOffset(1736); // 1688 factory default for my test chip
 }
